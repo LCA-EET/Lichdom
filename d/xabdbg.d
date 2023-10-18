c9f99b6aaf901e4b4b28767ec02a7b9332e6ba0b
practikit <LCAMod@danielvalle.net>
2023-10-17 22:36:50 -0400

    update

--- d/xabdbg.d
+++ d/xabdbg.d
@@ -5,6 +5,12 @@ IF ~
 ~ THEN BEGIN XA_DebugInit
 	SAY @0
 	
+	IF ~~ THEN REPLY @8
+	DO ~
+		ActionOverride(Player1, ReallyForceSpellRES("xaldlich", Myself))
+	~
+	EXIT
+	
 	IF ~~ THEN REPLY @1
 	DO ~
 		ReallyForceSpellRES("xaplich", Player1)
