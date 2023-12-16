dd51bd489cf525ba3deac7ed55f9d7583485d969
practikit <LCAMod@danielvalle.net>
2023-12-15 21:55:50 -0500

    update

---
+++ Release Notes.md
@@ -0,0 +1,4 @@
+## Lichdom Release Notes
+
+# v0.1.0 beta 2023-12-16
+* Initial testing release for EET.
---
+++ makePkg.ps1
@@ -0,0 +1,49 @@
+$basePath = "Lichdom_EET"
+$modPath = $basePath + "/" + $basePath
+$archive = $basePath + ".zip"
+$exePath = "setup-" + $basePath + ".exe"
+
+Remove-Item -LiteralPath $modPath -Force -Recurse
+Remove-Item $archive -Force
+Remove-Item $exePath -Force
+
+$folders = @(
+'2da',
+'baf',
+'bam',
+'bmp',
+'cre',
+'d',
+'itm',
+'spl',
+'tra',
+'wav'
+)
+
+foreach($folder in $folders){
+	Copy-Item -Path $folder -Destination ($modPath + "/" + $folder) -Recurse
+}
+
+Copy-Item -Path ($basePath + ".tp2") -Destination $modPath 
+Copy-Item -Path "license.md" -Destination $modPath
+
+Copy-Item -Path "weidu.exe" -Destination ($basePath + "/" + $exePath)
+Copy-Item -Path "Release Notes.md" -Destination ($basePath + "/Release Notes.md")
+
+$7zipPath = "$env:ProgramFiles/7-Zip/7z.exe"
+
+if (-not (Test-Path -Path $7zipPath -PathType Leaf)) {
+	$7zipPath = "F:/Program Files/7-Zip/7z.exe"
+}
+
+Set-Alias Start-SevenZip $7zipPath
+
+$Source = "./" + $basePath + "/*"
+$Target = "./" + $archive
+
+Start-SevenZip a -mx=9 $Target $Source
+
+Remove-Item -LiteralPath $basePath -Force -Recurse
+Get-FileHash $archive -Algorithm SHA256 > SHA256.txt
+
+Copy-Item -Path $archive -Destination "\\192.168.1.88\smbuser\Home\Installers\" + $archive
